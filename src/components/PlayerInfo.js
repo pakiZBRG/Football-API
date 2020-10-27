47660310d4b1a6c09334658cdb73a07be825bded
pakiZBRG <pavlovicnikola511@gmail.com>
2020-10-27 08:20:15 +0100

    fixed tables

--- src/components/PlayerInfo.js
+++ src/components/PlayerInfo.js
@@ -32,36 +32,37 @@ export default function PlayerInfo(props){
                 <table className='table-league'>
                     <thead>
                         <tr>
-                            <td className='center'></td>
-                            <td className='none'>Club</td>
-                            <td className='center'>P</td>
-                            <td className='center'>W</td>
-                            <td className='center'>D</td>
-                            <td className='center'>L</td>
-                            <td className='center'>Goals</td>
-                            <td className='center'>Diff</td>
-                            <td className='center'>Points</td>
-                            <td className='center'>Form</td>
+                            <td>Club</td>
+                            <td>Season</td>
+                            <td>League</td>
+                            <td>Games</td>
+                            {Player[0] && Player[0].position === 'Goalkeeper' ? <><td>Conceded</td><td>Saves</td></> : <><td>Goals (Penalty)</td></>}
+                            <td>Assists</td>
+                            <td>Passes</td>
+                            {Player[0] && Player[0].position === 'Midfielder' ? <><td>Pass %</td><td>Key Passes</td></> : null}
+                            <td>Red</td>
+                            <td>Yellow</td>
+                            {Player[0] && Player[0].position === 'Defender' ? <><td>Tackles</td><td>Interceptions</td><td>Duels (won)</td></> : null}
                         </tr>
                     </thead>
                     <tbody>
                         {Player.map(player => {
-                            const { cards, position, games, goals, passes, penalty, duels, shots, tackles, season, team_name, league } = player;
+                            const { cards, position, games, goals, passes, penalty, duels, tackles, season, team_name, league } = player;
                             return (
                                 <>
                                 {games.appearences !== 0 ? 
                                     <tr key={shortid.generate()}>
-                                        <td>{team_name} ({season})</td>
+                                        <td>{team_name}</td>
+                                        <td>{season}</td>
                                         <td>{league}</td>
-                                        <td>Games: {games.appearences}</td>
-                                        {position === 'Goalkeeper' ? null : <td><i className='fa fa-futbol'></i> {goals.total} ({penalty.success})</td>}
-                                        <td>Assists: {goals.assists}</td>
-                                        <td>Passes: {passes.total} {passes.accuracy}%</td>
-                                        {position === 'Midfielder' ? <td>Key Passes: {passes.key}</td> : null}
-                                        <td>Red: {cards.red} Yellow: {cards.yellow}</td>
-                                        {position === 'Goalkeeper' ? null : <td>Shots: {shots.total}</td>}
-                                        {position === 'Defender' ? <><td>Tackles: {tackles.total}</td><td>Interception: {tackles.interceptions}</td><td>Duels: {duels.total} Won: {duels.won}</td></> : null}
-                                        {position === 'Goalkeeper' ? <><td>Conceded: {goals.conceded}</td><td>Saves: {goals.saves}</td></> : null}
+                                        <td>{games.appearences}</td>
+                                        {position === 'Goalkeeper' ? <><td>{goals.conceded}</td><td>{goals.saves}</td></> : <><td><i className='fa fa-futbol'></i> {goals.total} ({penalty.success})</td></>}
+                                        <td>{goals.assists}</td>
+                                        <td>{passes.total}</td>
+                                        {position === 'Midfielder' ? <><td>{passes.accuracy}%</td><td>{passes.key}</td></> : null}
+                                        <td>{cards.red}</td>
+                                        <td>{cards.yellow}</td>
+                                        {position === 'Defender' ? <><td>{tackles.total}</td><td>{tackles.interceptions}</td><td>{duels.total} ({duels.won})</td></> : null}
                                     </tr> : null
                                 }   
                                 </>
